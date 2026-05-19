33189ad10de831dd0747824e6c91ed04acf16791
scwang09 <stanley.wang@twinhealth.com>
2026-05-19 11:21:00 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    serverComponentsExternalPackages: ["mammoth", "xlsx"],
-  },
+  serverExternalPackages: ["mammoth", "xlsx"],
 };
 
 module.exports = nextConfig;
